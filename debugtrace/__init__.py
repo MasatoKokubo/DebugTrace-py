cf115e972807dc5f2564de849b7b256097e52a70
MasatoKokubo <masatokokubo@gmail.com>
2020-01-06 07:10:19 +0900

    Update to 1.0.0a1

---
+++ debugtrace/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+# (C) 2020 Masato Kokubo
+from debugtrace.DebugTrace import *
+__version__ = '1.0.0a1'
---
+++ setup.py
@@ -0,0 +1,43 @@
+# setup.py
+# (C) 2020 Masato Kokubo
+from __future__ import absolute_import
+from __future__ import unicode_literals
+import os
+
+from setuptools import setup, find_packages
+
+try:
+    with open('README.rst') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+# version
+here = os.path.dirname(os.path.abspath(__file__))
+version = next((line.split('=')[1].strip().replace("'", '')
+                for line in open(os.path.join(here, 'debugtrace', '__init__.py'))
+                if line.startswith('__version__ = ')),
+               '0.0.0')
+
+setup(
+    name="debugtrace",
+    version=version,
+    url='https://github.com/MasatoKokubo/DebugTrace-python',
+    author='Masato Kokubo',
+    author_email='masatokokubo@gmail.com',
+    maintainer='Masato Kokubo',
+    maintainer_email='masatokokubo@gmail.com',
+    description='Output trace logs when debugging Python programs',
+    long_description=readme,
+    packages=find_packages(),
+    install_requires=[],
+    license="MIT",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
